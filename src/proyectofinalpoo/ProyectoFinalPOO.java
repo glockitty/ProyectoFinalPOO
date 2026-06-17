fedd1287608a07f8c2b4aaa50ef74b82c165c2ce
maiam <maiam@host.docker.internal>
2026-06-17 17:07:36 -0500

    First commit

---
+++ src/proyectofinalpoo/ProyectoFinalPOO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinalpoo;
+
+/**
+ *
+ * @author maiam
+ */
+public class ProyectoFinalPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hellooo");
+    }
+    
+}
